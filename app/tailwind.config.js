91f339e2f342d54d08ca01a0135d0ecb1b43c85e
Gregory Liu <gliu25@students.ubc.ca>
2024-11-23 03:59:47 -0800

    added tailwind css

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,14 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports =  {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Include all your React component files
-    "./index.html",       // Include your index.html
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
       colors: {
         primary: {
-          700: '#1E3A8A',
+          700: '#1E3A8A', // Example custom color
         },
       },
     },
